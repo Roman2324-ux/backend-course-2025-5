79ea25e07ff1daaffd2fcb9efb5f1d2063ea9cf9
Roman bc2024-5 <seaaes231@gmail.com>
2025-11-04 22:26:08 +0200

    Part 1

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
 
 program
-  .option('-p, --port <number>', 'port to run the server on')
-  .option('-t, --target <url>', 'target server URL');
-
+  .requiredOption('-p, --port <number>', 'server port')
+  .requiredOption('-h, --host <address>', 'server address')
+  .requiredOption('-c, --cache <path>', 'path to cache directory');
 program.parse(process.argv);
 
 const options = program.opts();
+const host = options.host;
+const port = options.port;
+const cacheDir = path.resolve(options.cache);
 
-console.log('Options received:', options);
+try {
+  if (!fs.existsSync(cacheDir)) {
+    fs.mkdirSync(cacheDir, { recursive: true });
+    console.log(`Cache directory created at: ${cacheDir}`);
+  } else {
+    console.log(`Using existing cache directory: ${cacheDir}`);
+  }
+} catch (err) {
+  console.error(`Failed to create cache directory: ${err.message}`);
+  process.exit(1);
+}
+
+const server = http.createServer((req, res) => {
+  res.writeHead(501, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Not Implemented\n');
+});
+
+server.listen(port, host, () => {
+  console.log(`Proxy server is running on http://${host}:${port}`);
+});
